3c1a6830d285ddf3f5caa9a94d54737fc3d52c77
Valeria Zamora <valezamora@gmail.com>
2018-09-07 16:27:46 -0600

    cambios por el enunciado

--- main.cpp
+++ main.cpp
@@ -12,24 +12,39 @@
 using namespace std;
  
 int main(int argc, char* argv[] ){
-    char* nomArchivo = (char *)"archivo.txt";
-    if(argc>1){
+    char* nomArchivo = (char *)"prueba.xml";
+    bool s = false;
+    if(argc>1){		//hay argumentos
+	if(argc == 2){
 		nomArchivo = argv[1];
+	}else{
+		if(string(argv[1]) == "-t"){
+			nomArchivo = argv[2];
+			s = true;
+		}
+			
 	}
-	
+    }
+
     GestorArchivos archivo(nomArchivo, 0);
-    char nombreSalida[] = "archivoSinEtiquetas.txt";
-    GestorArchivos salida(nombreSalida, 1);
     GestorEtiquetas etiquetas;
 
     string sigue;
+    string resultado;
     while(!archivo.fin()){
         sigue = etiquetas.extraerContenido(archivo.getNextLine());
-	
-	salida.escribirLinea(sigue);
-    }
-    
+	if(s){
+		resultado += sigue;		
+
+	}
+    }    
     etiquetas.imprimirEtiquetas();
+
+    if(s){
+	char nombreSalida[] = "archivoSinEtiquetas.txt";
+    	GestorArchivos salida(nombreSalida, 1);
+	salida.escribirLinea(resultado);
+    }
 
     return 0;
 }
